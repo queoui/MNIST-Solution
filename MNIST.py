d126c5ec7e00f037e39da60ae347f7a6a21b9036
mu11ern <mu11ern@yahoo.com>
2022-10-21 17:50:39 -0700

    final

--- MNIST.py
+++ MNIST.py
@@ -8,11 +8,11 @@ from sklearn.metrics import confusion_matrix
 def main():
 
     #experiment variables
-    learn_rate = 0.01
-    epochs = 4
-    hidden = 128
+    learn_rate = 0.1
+    epochs = 50
+    hidden = 100
     momentum_value = 0.9
-    batch = 1
+    batch = 30000
 
 
     #load and preprocess data
@@ -25,8 +25,8 @@ def main():
     X_train = X_train.reshape(-1, 784)
 
     #label targets
-    labels_train = np.zeros((len(Y_train),10), np.float32)
-    labels_train[range(labels_train.shape[0]),Y_train] = 1
+    labels_train = np.full((len(Y_train),10), 0.1, np.float32)
+    labels_train[range(labels_train.shape[0]),Y_train] = 0.9
 
     labels_test = np.zeros((len(Y_test),10), np.float32)
     labels_test[range(labels_test.shape[0]),Y_test] = 1
@@ -43,20 +43,16 @@ def main():
     train_accuracies, test_accuracies  = [] , []
     for epoch in range(epochs):
 
-        sample_train = np.random.randint(0, X_train.shape[0])
+        sample_train = np.random.randint(0, X_train.shape[0]-batch)
         sample_test = np.random.randint(0, X_test.shape[0])
 
         X_train_batch = X_train[sample_train:sample_train+batch]
         labels_train_batch = labels_train[sample_train:sample_train+batch]
-
-        X_test_batch = X_test[sample_test:sample_test+batch]
-        labels_test_batch = labels_test[sample_test:sample_test+batch]
-
         
         confusion_vector_category = []
         confusion_vector_truth = []
 
-        for digit, truth_vector in zip(X_train, labels_train):
+        for digit, truth_vector in zip(X_train_batch, labels_train_batch):
             #convert to vector
             digit.shape += (1,)
             truth_vector.shape += (1,)
@@ -79,17 +75,13 @@ def main():
 
             ho_weight += (momentum_value * (-learn_rate * delta_out @ np.transpose(h)))
             bias_ho += (momentum_value *(-learn_rate * delta_out))
-
             delta_out = out - truth_vector
-
             ho_weight += -learn_rate * delta_out @ np.transpose(h) 
             bias_ho += -learn_rate * delta_out
 
 
 
             # Backpropagation hidden -> input (activation function derivative)
-            # ih_weight += (momentum_value *(-learn_rate * delta_h @ np.transpose(digit)))
-            # bias_ih += momentum_value * (-learn_rate * delta_h )
             
             # momentum_h = momentum_value * delta_h
             delta_h = np.transpose(ho_weight) @ delta_out * (h * (1 - h))
@@ -123,8 +115,8 @@ def main():
 
 
         # Show accuracy for this epoch
-        print(f"Epoch: {epoch}:   Train Acc.: {round((train_correct / 60000) * 100, 2)}%  |  Test Acc: {round((test_correct / 10000) * 100, 2)}%")
-        train_accuracies.append(train_correct / 60000)
+        print(f"Epoch: {epoch}:   Train Acc.: {round((train_correct / batch) * 100, 2)}%  |  Test Acc: {round((test_correct / 10000) * 100, 2)}%")
+        train_accuracies.append(train_correct / batch)
         train_correct = 0
 
         test_accuracies.append(test_correct / 10000)
