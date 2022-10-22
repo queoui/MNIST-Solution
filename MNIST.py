423a44f924a5632a0277afa7739e758d50240568
mu11ern <mu11ern@yahoo.com>
2022-10-22 09:36:58 -0700

    done done

--- MNIST.py
+++ MNIST.py
@@ -1,4 +1,3 @@
-
 from array import array
 import numpy as np
 from keras.datasets import mnist
@@ -12,7 +11,7 @@ def main():
     epochs = 50
     hidden = 100
     momentum_value = 0.9
-    batch = 30000
+    batch = 60000
 
 
     #load and preprocess data
@@ -24,35 +23,40 @@ def main():
     X_test = X_test.reshape(-1, 784)
     X_train = X_train.reshape(-1, 784)
 
-    #label targets
+    #train label targets
     labels_train = np.full((len(Y_train),10), 0.1, np.float32)
     labels_train[range(labels_train.shape[0]),Y_train] = 0.9
 
-    labels_test = np.zeros((len(Y_test),10), np.float32)
-    labels_test[range(labels_test.shape[0]),Y_test] = 1
+    # test label targets
+    labels_test = np.full((len(Y_test),10), 0.1,  np.float32)
+    labels_test[range(labels_test.shape[0]),Y_test] = 0.9
 
 
-    #weight tables
+    #weight tables for input-?hidden, hidden->output, and their biases
     ih_weight = np.random.uniform(-0.05, 0.05, (hidden, 784))
     ho_weight = np.random.uniform(-0.05, 0.05, (10, hidden))
     bias_ih = np.random.uniform(-0.05, 0.05, (hidden, 1))
     bias_ho = np.random.uniform(-0.05, 0.05, (10, 1))
 
 
-
+    # for data plotting
     train_accuracies, test_accuracies  = [] , []
+
     for epoch in range(epochs):
 
-        sample_train = np.random.randint(0, X_train.shape[0]-batch)
+        sample_train = np.random.randint(0, X_train.shape[0])
         sample_test = np.random.randint(0, X_test.shape[0])
 
-        X_train_batch = X_train[sample_train:sample_train+batch]
-        labels_train_batch = labels_train[sample_train:sample_train+batch]
+        # used for smaller batches 
+        # X_train_batch = X_train[sample_train:sample_train+batch-batch]
+        # labels_train_batch = labels_train[sample_train:sample_train+batch]
         
+        # confusion matrix params
         confusion_vector_category = []
         confusion_vector_truth = []
 
-        for digit, truth_vector in zip(X_train_batch, labels_train_batch):
+        # train
+        for digit, truth_vector in zip(X_train, labels_train):
             #convert to vector
             digit.shape += (1,)
             truth_vector.shape += (1,)
@@ -67,14 +71,15 @@ def main():
             category = np.argmax(out)
             g_truth = np.argmax(truth_vector)
             
-
+            # correct amount
             train_correct += int(category == g_truth)
 
 
             # Backpropagation output -> hidden (cost function derivative)
-
+            # Add momentum
             ho_weight += (momentum_value * (-learn_rate * delta_out @ np.transpose(h)))
             bias_ho += (momentum_value *(-learn_rate * delta_out))
+
             delta_out = out - truth_vector
             ho_weight += -learn_rate * delta_out @ np.transpose(h) 
             bias_ho += -learn_rate * delta_out
@@ -82,13 +87,12 @@ def main():
 
 
             # Backpropagation hidden -> input (activation function derivative)
-            
-            # momentum_h = momentum_value * delta_h
             delta_h = np.transpose(ho_weight) @ delta_out * (h * (1 - h))
             ih_weight += -learn_rate * delta_h @ np.transpose(digit) 
             bias_ih += -learn_rate * delta_h 
 
 
+        # test
         for digit, truth_vector in zip(X_test, labels_test):
             #convert to vector
 
@@ -105,16 +109,16 @@ def main():
             category = np.argmax(out)
             g_truth = np.argmax(truth_vector)
 
-            # Cost / Error calculation
-            # e = 1 / len(out) * np.sum((out - truth_vector) ** 2, axis=0)
+            # correct
             test_correct += int(category == g_truth)    
 
+            # append for confusion matrix
             confusion_vector_category.append(category)
             confusion_vector_truth.append(g_truth)
 
 
 
-        # Show accuracy for this epoch
+        # Calculate accuracies, plot graph, plot confusion matrix and print. 
         print(f"Epoch: {epoch}:   Train Acc.: {round((train_correct / batch) * 100, 2)}%  |  Test Acc: {round((test_correct / 10000) * 100, 2)}%")
         train_accuracies.append(train_correct / batch)
         train_correct = 0
